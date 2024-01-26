adeaf8df729ac0b1e2bf43f2a102be2aa48233f9
ruud00000 <ruudnaastepad@hotmail.com>
2024-01-26 21:13:52 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 // import './registerServiceWorker'
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onOfflineReady() {
+    // Get the dialog element
+    const dialog = document.getElementById('offlineDialog');
+
+    // Show the dialog
+    dialog.style.display = 'block';
+
+    // Attach event listener to the OK button
+    const okButton = document.getElementById('okButton');
+    okButton.addEventListener('click', () => {
+      // Hide the dialog when OK button is clicked
+      dialog.style.display = 'none';
+    });
+  },
+  immediate: true
+});
+
 
 const app = createApp(App)
 
