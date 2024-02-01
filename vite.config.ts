5a578d0a8d75efca26a70e94613799bff8bc7820
ruud00000 <ruudnaastepad@hotmail.com>
2024-02-01 15:10:27 +0100

    upload to fu2.computerhuys working

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({  
+  /*server: {
+    proxy: {
+      '/api': 'http://fu2.computerhuys.nl', 
+    },
+  },*/
   plugins: [
     vue(),
     VitePWA({
@@ -502,4 +507,5 @@ export default defineConfig({
   build: {
     manifest: false,
   }
+
 })
